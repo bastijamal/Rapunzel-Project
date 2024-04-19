81ea21f6b891cd08b868c1fd37c528135de5ba9d
bastijamal <personal78acc@gmail.com>
2024-04-19 11:01:39 +0400

    kmkfc

---
+++ ConsoleApp1/ConsoleApp1/Hotel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Hotel
+	{
+
+		public string Name { get; set; }
+
+		public List<Room> rooms = new List<Room>();
+
+		public Hotel(string name) 
+		{
+			Name = name;
+		}
+
+		public void AddProduct(Room room)
+		{
+			rooms.Add(room);
+		}
+
+		public Room MakeReservation( int? roomId)
+		{
+			if(roomId==null) throw new NullReferenceException("ID SEHV DAXIL OLUNUB");
+
+			foreach (Room room in rooms)
+			{
+				if (room.IsAvialable)
+				{
+					Console.WriteLine("rezerv olundu");
+					room.IsAvialable = false;
+					Console.WriteLine(room);
+				}
+				else
+				{
+					Console.WriteLine("artiq rezerv olunub");
+				}
+			}
+			return null;
+
+		}
+
+=
+
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Room.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleApp1.Program;
+using System.Xml.Linq;
+using System.Data.SqlTypes;
+using System.Reflection.Metadata;
+
+namespace ConsoleApp1
+{
+	internal class Room
+	{
+
+		private static int _id;
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public int Price { get; set; }
+		public int PersonCapacity { get; set; }
+
+		public bool IsAvialable=true;
+
+		public Room(string name,int price, int perscap)
+		{
+			Id = ++_id;
+			Name = name;
+			Price = price;
+			PersonCapacity = perscap;
+			
+		}
+
+
+		public string ShowInfo()
+		{
+			return ($" ID: {Id} name: {Name} price: {Price} personcapacity:{PersonCapacity} isavailable:{IsAvialable}");
+
+		}
+
+		public override string ToString()
+		{
+			return ShowInfo();	
+		}
+
+
+
+
+
+	}
+}
